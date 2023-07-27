8ae2c39cd10b19dacb23135de5afa09f8f639b9c
Engel Dark <morenomoralesluisangel@gmail.com>
2023-07-27 14:26:05 -0500

    agregando url, para navegar entre template

--- backend/PrimeraApp/urls.py
+++ backend/PrimeraApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.Home, name = "saludar"),
+    path("", views.Home, name = "index"),
     path("contact/", views.Contactame, name="contactame"),
-    path("RedSocial/", views.RedesSociales, name="Red"),
+    path("RedSocial/", views.RedesSociales, name="Red_Social"),
 ]
