3dda2479ad62e4dde503002b7281fdb9fe2483c0
Engel Dark <morenomoralesluisangel@gmail.com>
2023-07-28 12:50:32 -0500

    creacion de modelos y migracion de ellos.

--- backend/PrimeraApp/models.py
+++ backend/PrimeraApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+#migrar tablas en sqlite3 python3 manage,py makemigrations 
+#python3 manage,py migrate
+
 # Create your models here.
+class Articulo(models.Model):
+    titulo = models.CharField(max_length=30)
+    contenido = models.TextField()
+    imagen = models.ImageField(default='null')
+    publico = models.BooleanField()
+    creacion = models.DateTimeField(auto_now_add=True)
+    actualizacion = models.DateTimeField(auto_now=True)
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=40)
+    descripcion = models.CharField(max_length=200)
+    creacion = models.DateField()
